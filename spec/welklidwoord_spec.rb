40985a6ff88d8d077ab1a3c0731f1bc8cf41dc3a
Max Shytikov <mshytikov@gmail.com>
2016-01-06 23:04:26 +0100

    Refactor Welklidwoord
    A bit of improvement in method name `is_het?` replaced with just
    'article' because it returns article.
    
    Also return value was replaced with symbols

--- spec/welklidwoord_spec.rb
+++ spec/welklidwoord_spec.rb
@@ -5,7 +5,22 @@ describe Welklidwoord do
     expect(Welklidwoord::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".article" do
+    subject { described_class.article(word) }
+
+    context "when word has 'het' article" do
+      let(:word) { "huis" }
+      it { is_expected.to be(:het) }
+    end
+
+    context "when word has 'de' article" do
+      let(:word) { "tafel" }
+      it { is_expected.to be(:de) }
+    end
+
+    context "when word is unknown" do
+      let(:word) { "brr" }
+      it { is_expected.to be(nil) }
+    end
   end
 end
